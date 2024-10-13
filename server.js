b36834f4fe833c962215f2ac5a967216e72c0450
Richard Bond <brycerbond@gmail.com>
2024-10-12 18:15:56 -0700

    Server now runs locally

--- server.js
+++ server.js
@@ -1,26 +1,45 @@
 // JavaScript source code
 require("dotenv").config();
-
-const arguments = process.argv.splice(2);
-var settings = {
-  webPort: process.env.SNAPIN_WEBPORT,
-};
 const fs = require("fs");
 const https = require("https");
-const cors = require("cors");
+const express = require("express");
+const expressSession = require("express-session");
+const cookieParser = require("cookie-parser");
 
-settings.webPort = arguments[0] ? arguments[0] : settings.webPort;
+// Probably should not have
+// const cors = require("cors");
 
-const options = {
-  key: fs.readFileSync(`${__dirname}/ssl/smartweb_key.pem`),
-  cert: fs.readFileSync(`${__dirname}/ssl/smartweb_crt.pem`),
+// Need to install
+// const passport = require("passport");
+// const flash = require("connect-flash");
+// const morgan = require("morgan");
+// const multiparty = require("connect-multiparty");
+
+// Other files that are not yet included
+// var httpMsgs = require("./app/httpmsgs");
+// require("./config/passport")(passport);
+// require("./routes/stripe.js")(stripe, passport);
+const { ENV, SNAPIN_WEBPORT } = process.env;
+const arguments = process.argv.splice(2);
+
+var settings = {
+  webPort: SNAPIN_WEBPORT,
 };
 
-const express = require("express");
-const passport = require("passport");
-const expressSession = require("express-session");
+settings.webPort = arguments[0] ? arguments[0] : settings.webPort;
+let options;
+if (ENV !== "dev") {
+  options = {
+    key: fs.readFileSync(`${__dirname}/ssl/smartweb_key.pem`),
+    cert: fs.readFileSync(`${__dirname}/ssl/smartweb_crt.pem`),
+  };
+}
 const app = express();
-app.use(cors());
+app.use(express.json());
+
+// This probably should be checked to make sure we still want this
+// app.use(cors());
+
 app.set("trust proxy", true);
 const appSession = expressSession({
   secret: "cdcfd1ad28c7455ba4d626ad50b9883b34f27502dfe22347f0b5fab2154a5754",
@@ -29,12 +48,14 @@ const appSession = expressSession({
   resave: false,
 });
 
-const bodyParser = require("body-parser");
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
-);
+// check what this setting does and figure out new way to do this with out bodyparser
+
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
+
 const blockedIps = [
   "192.168.1.1",
   "10.0.2.139",
@@ -53,17 +74,21 @@ app.use((req, res, next) => {
     next();
   }
 });
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
 
-app.use(appSession);
-app.use(passport.initialize());
-app.use(passport.session(appSession));
+// check what this setting does and figure out new way to do this with out bodyparser
 
-require("./config/passport")(passport);
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+// app.use(bodyParser.json({ limit: "50mb" }));
+
+// Needs to be added in again when files are ready
+
+// app.use(appSession);
+// app.use(passport.initialize());
+// app.use(passport.session(appSession));
 
 app.set("view engine", "ejs");
 
+// This doesn't seem to be called anywhere?
 const getClientAddress = function (req) {
   return (
     (req.headers["x-forwarded-for"] || "").split(",")[0] ||
@@ -71,47 +96,52 @@ const getClientAddress = function (req) {
   );
 };
 
-const morgan = require("morgan");
-const multiparty = require("connect-multiparty");
-const multipartyMiddleware = multiparty();
+// Check package and see if needed to be reinstalled
 
-app.use(multipartyMiddleware);
-
-const cookieParser = require("cookie-parser");
-const flash = require("connect-flash");
-var httpMsgs = require("./app/httpmsgs");
+// const multipartyMiddleware = multiparty();
+// app.use(multipartyMiddleware);
 
 app.get("/robots.txt", function (req, res) {
   res.type("text/plain");
   res.send("User-agent: *\nDisallow: /");
 });
 
-app.use("/sites", express.static(`${__dirname}/sites`));
-app.use("/secure", express.static(`${__dirname}/sites`));
-app.use("/secure/sites", express.static(`${__dirname}/sites`));
-app.use("/secure/smartwebpro9", express.static(`${__dirname}/sites`));
+// Check to make sure we need this
+
+// app.use("/sites", express.static(`${__dirname}/sites`));
+// app.use("/secure", express.static(`${__dirname}/sites`));
+// app.use("/secure/sites", express.static(`${__dirname}/sites`));
+// app.use("/secure/smartwebpro9", express.static(`${__dirname}/sites`));
 //app.use("/stripe", express.static(`${__dirname}/stripe`));
 
-app.use(morgan("dev"));
+// check if we need this
+
+// app.use(morgan("dev"));
+
 app.use(cookieParser());
 app.use(express.static(`${__dirname}/public`));
-app.use(flash());
+
+// check if needed
+// app.use(flash());
 
 app.get("/healthcheck", (req, res) => {
-  console.log("PORT: ", process.env.SNAPIN_WEBPORT);
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.write("loaderio-f77de18d9ec250150dd814d3933b2026");
   res.end();
 });
-app.get("/", (req, res, next) => {
-  const hostname = req.hostname;
-  console.log(hostname);
-  if (req.hostname === "www.quantrex.com") {
-    const endpoint = `${req.protocol}://gtm.smartweb-pro.com/sites/opensite/-141`;
-    return res.redirect(endpoint);
-  }
-  next();
-});
+
+// This may need to be conditional based on if not dev?
+
+// app.get("/", (req, res, next) => {
+//   const hostname = req.hostname;
+//   console.log(hostname);
+//   if (req.hostname === "www.quantrex.com") {
+//     const endpoint = `${req.protocol}://gtm.smartweb-pro.com/sites/opensite/-141`;
+//     return res.redirect(endpoint);
+//   }
+//   next();
+// });
+
 app.get("/", function (req, res) {
   res.render("auth/splash.ejs", { user: null });
 });
@@ -119,15 +149,15 @@ app.get("/home", function (req, res) {
   res.render("auth/splash.ejs", { user: req.user });
 });
 
-app.get("/logout", function (req, res) {
-  if (req._passport.session && req._passport.session.user) {
-    req.logout();
-  }
-  req.user = null;
-  req.session.destroy(); // Destroy session
-  res.clearCookie("connect.sid"); // Clear session cookie
-  res.redirect("/");
-});
+// app.get("/logout", function (req, res) {
+//   if (req._passport.session && req._passport.session.user) {
+//     req.logout();
+//   }
+//   req.user = null;
+//   req.session.destroy(); // Destroy session
+//   res.clearCookie("connect.sid"); // Clear session cookie
+//   res.redirect("/");
+// });
 // API ROUTES
 
 // PUBLIC ROUTES
@@ -137,7 +167,6 @@ app.get("/logout", function (req, res) {
 // PROTECTED ROUTES
 
 // const stripe = express.Router();
-// require("./routes/stripe.js")(stripe, passport);
 // app.use("/stripe", stripe);
 
 app.use((req, res, next) => {
@@ -163,11 +192,18 @@ app.use((req, res, next) => {
       );
   }
 });
-
+// WE Should look into a dedicated logger for logs, set different levels of logging
+// Look into Winston.js
 app.use(function (req, res) {
   console.log(req.socket.remoteAddress);
 });
 
-https.createServer(options, app).listen(settings.webPort, function () {
-  console.log("Express server listening on port " + settings.webPort);
-});
+if (ENV === "dev") {
+  app.listen(SNAPIN_WEBPORT, () => {
+    console.log(`Example app listening on port ${SNAPIN_WEBPORT}`);
+  });
+} else {
+  https.createServer(options, app).listen(settings.webPort, function () {
+    console.log("Express server listening on port " + settings.webPort);
+  });
+}
