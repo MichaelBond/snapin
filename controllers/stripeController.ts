c37599a7dc0c0aaf486f70071a8c320e18deb7ee
Richard Bond <brycerbond@gmail.com>
2024-10-14 12:17:40 -0700

    removed console.log

--- controllers/stripeController.ts
+++ controllers/stripeController.ts
@@ -1,9 +1,8 @@
 import StripeServiceClass from "../services/StripeClass";
 import configs from '../configs/config'
 const { STRIPE_API_KEY } = configs
-console.log(STRIPE_API_KEY)
 
 const stripeService = new StripeServiceClass(STRIPE_API_KEY)
 export const getBalance = async () => {
-        return await stripeService.getBalance()
+    return await stripeService.getBalance()
 }
