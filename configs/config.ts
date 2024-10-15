632598f6825f57f0caa4adc78adf3fb11dc91e4e
Michael Bond <mike@quantrex.com>
2024-10-14 17:24:56 -0700

    broken

--- configs/config.ts
+++ configs/config.ts
@@ -10,7 +10,25 @@ const configs: Record<Environment, any> = {
         SNAPIN_SESSION_SECRET: process.env.SNAPIN_SESSION_SECRET,
         SNAPIN_WEBPORT: process.env.SNAPIN_WEBPORT,
         STRIPE_API_KEY: process.env.STRIPE_API_KEY,
-        ENV: env
+        ENV: env,
+
+        MSSQL_QUANTREX: {
+            server: process.env.QUANTREX_SERVER,
+            database: process.env.QUANTREX_DATABASE,
+            user: process.env.QUANTREX_USER,
+            password: process.env.QUANTREX_PASSWORD,
+            port: process.env.QUANTREX_PORT,
+            requestTimeout: process.env.QUANTREX_REQUEST_TIMEOUT,
+            pool: {
+              max: process.env.QUANTREX_POOL_MAX,
+              min: process.env.QUANTREX_POOL_MIN,
+              idleTimeoutMillis: process.env.QUANTREX_IDLE_TIMEOUT
+            },
+            options: {
+              trustServerCertificate: true,
+              enableArithAbort: true,
+            }
+         }
     },
     test: {},
     prod: {}
