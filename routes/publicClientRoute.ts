c2b3b48ba1e883209d435118fbaefcf7439b43a8
Michael Bond <mike@quantrex.com>
2024-10-21 11:55:36 -0700

    Add chatbot ejs

--- routes/publicClientRoute.ts
+++ routes/publicClientRoute.ts
@@ -41,6 +41,9 @@ router.get("/change-password", function (req, res) {
 router.get("/chat", function (req, res) {
     res.render("public/chat.ejs", { user: req?.user, message: "Chat" });
 });
+router.get("/chatbot", function (req, res) {
+    res.render("public/chatbot.ejs", { user: req?.user, message: "Chat" });
+});
 router.get("/reset-password", function (req, res) {
     res.render("auth/resetpassword.ejs", { user: req?.user, message: "Reset Password" });
 });
